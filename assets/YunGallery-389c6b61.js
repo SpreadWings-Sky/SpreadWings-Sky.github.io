a67aad71edc008715ac8e145aa4cadf2fa3d2c24
SpreadWings-Sky <SpreadWings-Sky@users.noreply.github.com>
2023-04-25 17:19:21 +0000

    deploy: SpreadWings-Sky/Blog@273fb351d0c979d90ba3f7461ace2fbc83550a46

---
+++ assets/YunGallery-389c6b61.js
@@ -0,0 +1,1 @@
+import{g as n,l as t,o as l,c as s}from"./app-a9f4ec8a.js";const c=n({__name:"YunGallery",props:{photos:null},setup(o){return(a,p)=>{const e=t("VAGallery");return l(),s(e,{photos:o.photos},null,8,["photos"])}}});export{c as default};
