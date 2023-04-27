186a4521e201175f9253a937334ae415884cce1d
SpreadWings-Sky <SpreadWings-Sky@users.noreply.github.com>
2023-04-27 10:26:09 +0000

    deploy: SpreadWings-Sky/Blog@637047582707a7faeb7148e45c79cea08f9530bc

---
+++ assets/_...all_-46a00ce7.js
@@ -0,0 +1,1 @@
+import{g as a,Y as s,Z as n,$ as r,h as c,o as p}from"./app-594a6ef0.js";import{b as o}from"./route-block-83d24a4e.js";const u=a({__name:"[...all]",setup(f){const t=s(),e=n();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&t.replace(e.path.slice(0,-1))}),(i,l)=>(p(),c("div"))}});typeof o=="function"&&o(u);export{u as default};
