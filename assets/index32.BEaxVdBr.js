7fccb0363f43a2700419a8f4346774238f99f7f0
SpreadWings-Sky <SpreadWings-Sky@users.noreply.github.com>
2026-07-25 09:55:23 +0000

    deploy: SpreadWings-Sky/Blog@9f29009f6c025858f3b2f534a450567693dc729f

---
+++ assets/index32.BEaxVdBr.js
@@ -0,0 +1,1 @@
+import{$ as e,W as t}from"./theme.LNLpEuui.js";import{p as n}from"./index3.qjfU20XX.js";import{t as r}from"./schema-org.Ba7D0Hp1.DOT_aECu.js";var i=t({defaults:{"@type":`Review`},inheritMeta:[`inLanguage`],resolve(t,i){return t.reviewRating=e(t.reviewRating,i,r),t.author=e(t.author,i,n),t}});export{i as reviewResolver};
